196cca6aac2a0549ec5114a6b7d397f31ce41ac2
KyleLan329 <kunlan3-c@my.cityu.edu.hk>
2017-12-25 21:46:57 +0800

    update

---
+++ www/orm.py
@@ -0,0 +1,10 @@
+import aiomysql, asyncio, logging
+from fields import Field
+logging.basicConfig(level=logging.INFO)
+
+#Print SQL
+def log(sql, args=()):
+    logging.info('SQL: %s,%s' % (sql,args))
+
+#Create a global connect pool
+async def create_pool(loop, **kw) 

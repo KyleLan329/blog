b69a9bbc1595049d3a32287658387c018028585d
KyleLan329 <kunlan3-c@my.cityu.edu.hk>
2017-12-27 00:08:56 +0800

    sql test

---
+++ www/SQLTest.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import orm, asyncio
+
+from models import User, Blog, Comment
+
+async def test(loop):
+    await orm.create_pool(loop, user='www-data', password='www-data', db='awesome')
+    u = User(name='Kyle', email='kylelan329@qq.com', passwd='123456', image='about:blank')
+    await u.save()
+
+if __name__ == '__main__':
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete( asyncio.wait([test(loop)]) )  
+    loop.close()
+    if loop.is_closed():
+        sys.exit(0)

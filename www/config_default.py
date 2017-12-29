10b1aa47b9a9c113335fb193ffceb237777fdb71
sx_lankun <sx_lankun@zhongan.com>
2017-12-29 20:09:17 +0800

    update

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'lantern',
+        'password': 'lanternblog',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
